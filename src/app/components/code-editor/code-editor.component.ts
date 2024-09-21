152c02e78eddce1ebe155428820d8f2d97688a7e
Adriano Ferreira <adriano.13lp@gmail.com>
2024-09-21 10:20:45 -0300

    refactor: refatoração de alguns componentes.

--- src/app/components/code-editor/code-editor.component.ts
+++ src/app/components/code-editor/code-editor.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { languages, editorOptions } from '../../config/editor.config';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { FormsModule } from '@angular/forms';
@@ -11,25 +11,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './code-editor.component.css',
 })
 export class CodeEditorComponent {
-  isBrowser = false;
   languages = languages;
   editorOptions = editorOptions;
   selectedLanguage = this.languages[0].value;
   code: string = this.languages[0].template;
 
-  constructor() {
-    afterNextRender(() => {
-      this.isBrowser = true;
-    });
-  }
-
-  changeLanguage(event: any) {
-    const selected = this.languages.find(
-      (language) => language.value === event.target.value
+  onLanguageChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const selectedLanguage = this.languages.find(
+      (language) => language.value === target.value
     );
-    if (selected) {
-      this.selectedLanguage = selected.value;
-      this.code = selected.template;
+
+    if (selectedLanguage) {
+      this.selectedLanguage = selectedLanguage.value;
+      this.code = selectedLanguage.template;
       this.editorOptions = {
         ...this.editorOptions,
         language: this.selectedLanguage,
--- src/app/pages/create-snippet/create-snippet.component.ts
+++ src/app/pages/create-snippet/create-snippet.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CodeEditorComponent } from '../../components/code-editor/code-editor.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-create-snippet',
   standalone: true,
-  imports: [CodeEditorComponent],
+  imports: [CodeEditorComponent, HeaderComponent],
   templateUrl: './create-snippet.component.html',
   styleUrl: './create-snippet.component.css',
 })
